d11a9470917593f9f8f81ac2ed84dc5257a7f81d
Tet <anabathula.praveen103@gmail.com>
2023-03-21 06:57:34 +0530

    Update env variable for deployment

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ require("dotenv").config();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = `mongodb+srv://tet:${process.env.DATABASE_PASSOWORD}@cluster0.djbois6.mongodb.net/mini-message-board?retryWrites=true&w=majority`;
+const mongoDB = `mongodb+srv://tet:${{
+  DATABASE_PASSOWORD,
+}}@cluster0.djbois6.mongodb.net/mini-message-board?retryWrites=true&w=majority`;
 
 main().catch((err) => console.log(err));
 async function main() {
